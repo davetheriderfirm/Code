e03c7d3b80b2fb7609edf8bf5249a492df6bcf3a
dave brothers <dave@theriderfirm.cc>
2023-07-29 21:59:34 +0100

    Chapter 14

---
+++ chapter 14.py
@@ -0,0 +1,18 @@
+import os
+
+def walk(dirname):
+    for name in os.listdir(dirname):
+        path = os.path.join(dirname, name)
+        if os.path.isfile(path):
+            print(path)
+        else:
+            walk(path)
+
+def os_walk(dirname):
+    for root, dirs, files in os.walk(cwd):
+        for filename in files:
+            print(os.path.join(root,filename))
+
+cwd = os.getcwd()
+print(cwd)
+os_walk(cwd)
