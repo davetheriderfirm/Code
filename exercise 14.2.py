3d7b6d8ab9595cb45ec1393d7554d233991a4be9
dave brothers <dave@theriderfirm.cc>
2023-07-30 19:32:13 +0100

    more

--- exercise 14.2.py
+++ exercise 14.2.py
@@ -1,1 +1,11 @@
 import anagram_sets
+import dbm
+import pickle
+
+def store_anagrams(anag_map):
+    for key, value in anagram_map.items():
+        db[key] = pickle.dumps(value)
+        
+db = dbm.open('myanagrams', 'c')
+anagram_map = anagram_sets.all_anagrams('words.txt')
+store_anagrams(anagram_map)
