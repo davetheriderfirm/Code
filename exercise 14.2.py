ff49843130ca3083cf0de2f369b184e8d149e710
dave brothers <dave@theriderfirm.cc>
2023-07-30 19:34:07 +0100

    more

--- exercise 14.2.py
+++ exercise 14.2.py
@@ -5,7 +5,19 @@ import pickle
 def store_anagrams(anag_map):
     for key, value in anagram_map.items():
         db[key] = pickle.dumps(value)
+
+def read_anagrams(inword):
+    try:
+        outlist = db[inword]
+    except:
+        print(inword + ' is not in the wordlist.')
+        return
+    print(pickle.loads(outlist))
         
 db = dbm.open('myanagrams', 'c')
 anagram_map = anagram_sets.all_anagrams('words.txt')
 store_anagrams(anagram_map)
+
+read_anagrams('nest')
+
+db.close()
