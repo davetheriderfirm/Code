ebabb78b8ca4f42604c1153a24502341d20026d8
dave brothers <dave@theriderfirm.cc>
2023-08-31 22:31:02 +0100

    chapter 17

--- chapter 17.py
+++ chapter 17.py
@@ -9,12 +9,22 @@ class Point:
         return 'x = %.5f ,y = %.5f' % (self.x, self.y)
 
     def __add__(self, other: "Point") -> "Point":
-        p_add = Point(self.x + other.x, self.y + other.y)
-        return p_add
+        if isinstance(other, Point):
+            return(Point(self.x + other.x, self.y + other.y))
+        elif isinstance(other, tuple):
+            return(Point(self.x + other[0], self.y + other[1]))
+        else:
+            raise TypeError('Invalid object in Point __add__')
+        
+    def __radd__(self, other):
+        return self.__add__(other)
         
 
-p1 = Point(31.2, 5)
+p1 = Point(31.2, 5.0)
 print(p1)
-p2 = Point(11, -6.77)
+p2 = Point(11.0, -6.2)
 print(p2)
 print(p1 + p2)
+p3 = Point(-4.0, 33.0)
+print(p1+p2+p3)
+total = sum([p1, p2, p3])
