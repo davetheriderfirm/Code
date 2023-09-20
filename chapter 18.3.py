7f14ceacde9064af3fa750058451d8d048511671
dave brothers <dave@theriderfirm.cc>
2023-09-20 20:01:45 +0100

    ch18

--- chapter 18.3.py
+++ chapter 18.3.py
@@ -1,4 +1,116 @@
-import Card
+import random
+
+
+class Card(object):
+    """Represents a standard playing card.
+    
+    Attributes:
+      suit: integer 0-3
+      rank: integer 1-13
+    """
+
+    suit_names = ["Clubs", "Diamonds", "Hearts", "Spades"]
+    rank_names = [None, "Ace", "2", "3", "4", "5", "6", "7", 
+              "8", "9", "10", "Jack", "Queen", "King"]
+
+    def __init__(self, suit=0, rank=2):
+        self.suit = suit
+        self.rank = rank
+
+    def __str__(self):
+        """Returns a human-readable string representation."""
+        return '%s of %s' % (Card.rank_names[self.rank],
+                             Card.suit_names[self.suit])
+    
+    def __lt__(self,other):
+        t1 = self.suit, self.rank
+        t2 = other.suit, other.rank
+        return t1 < t2
+
+    def __cmp__(self, other):
+        """Compares this card to other, first by suit, then rank.
+
+        Returns a positive number if this > other; negative if other > this;
+        and 0 if they are equivalent.
+        """
+        t1 = self.suit, self.rank
+        t2 = other.suit, other.rank
+        return t1 > t2
+
+
+class Deck(object):
+    """Represents a deck of cards.
+
+    Attributes:
+      cards: list of Card objects.
+    """
+    
+    def __init__(self):
+        self.cards = []
+        for suit in range(4):
+            for rank in range(1, 14):
+                card = Card(suit, rank)
+                self.cards.append(card)
+
+    def __str__(self):
+        res = []
+        for card in self.cards:
+            res.append(str(card))
+        return '\n'.join(res)
+
+    def add_card(self, card):
+        """Adds a card to the deck."""
+        self.cards.append(card)
+
+    def remove_card(self, card):
+        """Removes a card from the deck."""
+        self.cards.remove(card)
+
+    def pop_card(self, i=-1):
+        """Removes and returns a card from the deck.
+
+        i: index of the card to pop; by default, pops the last card.
+        """
+        return self.cards.pop(i)
+
+    def shuffle(self):
+        """Shuffles the cards in this deck."""
+        random.shuffle(self.cards)
+
+    def sort(self):
+        """Sorts the cards in ascending order."""
+        self.cards.sort()
+
+    def move_cards(self, hand, num):
+        """Moves the given number of cards from the deck into the Hand.
+
+        hand: destination Hand object
+        num: integer number of cards to move
+        """
+        for i in range(num):
+            hand.add_card(self.pop_card())
+
+
+class Hand(Deck):
+    """Represents a hand of playing cards."""
+    
+    def __init__(self, label=''):
+        self.cards = []
+        self.label = label
+
+
+def find_defining_class(obj, method_name):
+    """Finds and returns the class object that will provide 
+    the definition of method_name (as a string) if it is
+    invoked on obj.
+
+    obj: any python object
+    method_name: string method name
+    """
+    for ty in type(obj).mro():
+        if method_name in ty.__dict__:
+            return ty
+    return None
 
 
 class PokerHand(Hand):
@@ -22,8 +134,41 @@ class PokerHand(Hand):
             if val >= 5:
                 return True
         return False
+    
+    def rank_hist(self):
+        """Builds a histogram of the ranks that appear in the hand.
 
+        Stores the result in attribute ranks.
+        """
+        self.ranks = {}
+        for card in self.cards:
+            self.ranks[card.rank] = self.ranks.get(card.rank, 0) + 1
 
+    def has_pair(self):
+        """Returns True if hand has a pair, False otherwise."""
+        self.rank_hist()
+        for val in self.ranks.values():
+            if val >= 2:
+                return True
+        return False    
+    
+    def has_two_pair(self):
+        self.rank_hist()
+        pairtot = 0
+        for val in self.ranks.values():
+            if val >= 2:
+                pairtot += 1
+        if pairtot > 1:
+            return True
+        return False  
+
+    def has_three_kind(self):
+        self.rank_hist()
+        for val in self.ranks.values():
+            if val >= 3:
+                return True
+        return False
+        
 if __name__ == '__main__':
     # make a deck
     deck = Deck()
@@ -35,5 +180,8 @@ if __name__ == '__main__':
         deck.move_cards(hand, 7)
         hand.sort()
         print(hand)
-        print(hand.has_flush)
+        print('Flush', hand.has_flush())
+        print('Pair', hand.has_pair())
+        print('Two Pairs', hand.has_two_pair())
+        print('Three of a Kind', hand.has_three_kind())
         print('')
