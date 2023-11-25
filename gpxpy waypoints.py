c59f0d99cb81dd2e0c6aa09a1fca21355a925406
dave brothers <dave@theriderfirm.cc>
2023-11-25 17:40:24 +0000

    GPX

--- gpxpy waypoints.py
+++ gpxpy waypoints.py
@@ -1,5 +1,13 @@
 import gpxpy
 
+# This utility reads in a GPX file with waypoints and outputs an updated version in a format that 
+# allows a Wahoo Elemnt Bolt GPS to use the waypoints as proximity alerts when traversing the track.
+# 1. Parse the input file as a GPX using gpxpy
+# 2. Sort the waypoints by description, so that they are in the correct order they will be passed when traversing the track. 
+# 3. For each waypoint, find the first trackpoint that is within 200m of the waypoint. Update the lon/lat co-ordinates
+# of the waypoint to match those of the trackpoint.
+# 4. Write the output file from the updated GPX holder.
+
 def wp_distance(wp, tp, radius_metres):
     '''Check whether the distance between the waypoint and the trackpoint is within the radius'''
     wp_dist = gpxpy.geo.haversine_distance(wp.latitude, wp.longitude, tp.latitude, tp.longitude)
@@ -12,10 +20,10 @@ def find_match(wp, tr_index):
     '''Loop through the trackpoints starting at the current index, checking whether the distance between the
        waypoint and the current trackpoint is within the radius.
        Returns the index needed for starting the next check'''
-    while tr_index < tot_points: 
+    while tr_index < len(tracklist): 
         print(tr_index)
         if wp_distance(waypoint, tracklist[tr_index],200):
-# If we have a match, update the waypoint, then increment the index so that next time this function is called, the 
+# We have a match, update the waypoint, return the index so that next time this function is called, the 
 # search begins from the next trackpoint rather than from the beginning again.
 # We know this will work because we have ordered the waypoints already.            
             print(waypoint)        
@@ -39,16 +47,16 @@ gpx.waypoints.sort(key=lambda waypoint: waypoint.description)
 
 # Count the number of points
 # Generally our input file will have a single track with a single segment, but handle the case of multiple.
-tot_points = 0
+
 tracklist = []
 for track in gpx.tracks:
     for segment in track.segments:
-        tot_points += len(segment.points)
         for point in segment.points:
             tracklist.append(point)
 
 
 # For each waypoint, traverse through the track looking for a point that is with a certain radius (in metres) of the waypoint.
+# find_match returns the current position in the track so that we don't start from the beginning each time.
 trackindex = 0
 for waypoint in gpx.waypoints:
     print(waypoint.description)
@@ -58,6 +66,6 @@ for waypoint in gpx.waypoints:
    
 
 
-# Save the modified GPX data to a new file
-with open('waypoint test fixed.gpx', 'w') as gpx_file:
+# Write the modified GPX data to a new file
+with open('output.gpx', 'w') as gpx_file:
     gpx_file.write(gpx.to_xml())
